358fecb45a58502cd7d25a2b28fafdde343d028e
Sungjin Kim <68371801+kimsj5259@users.noreply.github.com>
2023-02-21 14:49:17 +0900

    trial3

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.54.0"
+    }
+  }
+}
 provider "aws" {
   region     = var.region
   # Hard-coding credentials is not recommended
