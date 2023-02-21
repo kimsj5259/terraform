a028782a4b4d8b6c8213120219f40e4c663b8bbb
Sungjin Kim <68371801+kimsj5259@users.noreply.github.com>
2023-02-21 14:59:43 +0900

    trial4

--- configuration.tf
+++ configuration.tf
@@ -16,8 +16,8 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami = "ami-0a5a6128e65676ebb"
   instance_type = "t2.micro"
-}
 
-output "ec2_ip" {
-    value = aws_instance.ec2.private_ip
+  tags = {
+    Name = "trial-instance"
+  }
 }
