fbab124c8499d8818a0382c4522d55628bc21744
Sungjin Kim <68371801+kimsj5259@users.noreply.github.com>
2023-02-21 15:49:35 +0900

    trial6

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.54.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = var.region
   # Hard-coding credentials is not recommended
@@ -5,8 +14,8 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_instance" "ec2" {
-  ami = "ami-0a5a6128e65676ebb"
+resource "aws_instance" "example" {
+  ami = "ami-0454bb2fefc7de534"
   instance_type = "t2.micro"
 
   tags = {
